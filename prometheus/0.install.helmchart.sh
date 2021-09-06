3d705829c2588bdf316ba6c5af6b3c0d0ad2cc01
rooty <rooty@vps7>
2021-09-07 00:42:32 +0200

    wip

---
+++ prometheus/0.install.helmchart.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Paulo Aleixo Campos
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+function shw_info { echo -e '\033[1;34m'"$1"'\033[0m'; }
+function error { echo "ERROR in ${1}"; exit 99; }
+trap 'error $LINENO' ERR
+PS4='████████████████████████${BASH_SOURCE}@${FUNCNAME[0]:-}[${LINENO}]>  '
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+
+
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add kube-state-metrics https://kubernetes.github.io/kube-state-metrics
+helm repo update
+helm install prometheus prometheus-community/prometheus
